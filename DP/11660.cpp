a67865f209e13c2a0dfe6516707a42db92beaa99
KwonET <dlxl3652zz@gmail.com>
2023-10-19 16:40:32 +0900

    [ 1019 ] : 구간 합 구하기 5

---
+++ DP/11660.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <cstdio>
+#include <algorithm>
+using namespace std;
+int n,m,A[1025][1025],i,j,r,c,x1,y1,x2,y2,result;
+int main(){
+    scanf("%d %d",&n,&m);
+    for(i=0;i<n;i++){
+        for(j=0;j<n;j++){
+            scanf("%d",&A[i][j]);
+        }
+    }
+    for(r=0;r<n;r++){
+        for(c=0;c<n;c++){
+            if(r==0 && c==0){
+                continue;
+            }
+            if(r==0){
+                A[r][c]=A[r][c-1]+A[r][c]; continue;
+            }
+            if(c==0){
+                A[r][c]=A[r-1][c]+A[r][c]; continue;
+            }
+            A[r][c]=A[r][c-1]+A[r-1][c]-A[r-1][c-1]+A[r][c];
+        }  
+    }
+        
+    for(i=0;i<m;i++){
+        scanf("%d %d %d %d",&y1,&x1,&y2,&x2);
+        y1-=1;x1-=1;y2-=1;x2-=1;
+        if(x1==0 && y1==0){
+            result=A[y2][x2]; 
+            printf("%d\n",result); continue;
+        }
+        if(x1==0){
+            result=A[y2][x2]-A[y1-1][x2];
+            printf("%d\n",result); continue;
+        }
+        if(y1==0){
+            result=A[y2][x2]-A[y2][x1-1];
+            printf("%d\n",result); continue;
+        }
+        result=A[y2][x2]-A[y2][x1-1]-A[y1-1][x2]+A[y1-1][x1-1];
+        printf("%d\n",result);
+    }
+    return 0;
+}
