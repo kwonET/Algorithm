fcd288446e997d767b71316d9a078719814b053d
KwonET <dlxl3652zz@gmail.com>
2023-12-06 13:57:22 +0900

    [BOJ] 보석도둑 / 골드 2 / 2시간

---
+++ 그리디/1202.cpp
@@ -0,0 +1,46 @@
+// deque로 끝값을 빼는 대신, pq로 계속해서 순서를 유지해서 이진탐색으로 해당값을 pop할 수 있는 방법이 있다.
+#include <iostream>
+#include <vector>
+#include <deque>
+#include <algorithm>
+#include <queue>
+using namespace std;
+int jw,bag,i,a,b,c;
+
+vector<pair<int,int>> jwInfo; //보석의 무게, 보석의 가격
+vector<int> bagInfo; //가방의 무게
+priority_queue<int> pq;
+struct compare{
+    bool operator()(pair<int,int> &a, pair<int,int> &b){
+        return (a.second<b.second); // * 순서 확인하기
+    }
+};
+int main(){
+    cin>>jw>>bag;
+    for(i=0;i<jw;i++){
+        cin>>a>>b;
+        jwInfo.push_back({a,b});
+    }
+    for(i=0;i<bag;i++){
+        cin>>c;
+        bagInfo.push_back(c);
+    }
+    sort(jwInfo.begin(),jwInfo.end()); //왜 sort를 빼도 맞는건지?
+    sort(bagInfo.begin(),bagInfo.end());
+    
+    int idx=0;
+    long long totaljw=0;
+    for(auto bags:bagInfo){
+        while(idx<jw && bags>=jwInfo[idx].first){
+            pq.push(jwInfo[idx].second);
+            idx++;
+            continue; //왜 continue를 빼도 맞는건지?
+        } 
+        if(!pq.empty()){
+            totaljw+=pq.top();
+            pq.pop();
+        }
+    }
+    cout<<totaljw<<endl;
+    return 0;
+}
