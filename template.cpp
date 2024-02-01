0a840df616022004e1e5f648d242fb5d46c261f2
KwonET <dlxl3652zz@gmail.com>
2024-02-01 20:52:46 +0900

    코드 템플릿

---
+++ template.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll=long long;
+using pii =pair<int,int>;
+using pll=pair<ll,ll>;
+#define all(v) v.begin(),v.end()
+
+void solve(){
+
+}
+
+int main(void){
+    ios::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+    int T=1; //cin>>T;
+    while(T--) solve();
+    return 0;
+}
